b106c20c3522aac5bd67d59568bd50255752ed52
IN\janecka <tomas.janecka@thermofisher.com>
2025-12-04 11:31:51 +0100

    Working code with small input

--- 5/c/aoc_task.h
+++ 5/c/aoc_task.h
@@ -3,13 +3,17 @@
 
 #include <stdio.h>
 
+#define ADJACENT_FLAT_POSITIONS_COUNT 9
+#define ADJACENT_THRESHOLD 4
+
 typedef struct PaperRoll {
-    unsigned int row_position;
-    unsigned int column_position;
+    int row_position;
+    int column_position;
+    int adjacent_count;
 } PaperRoll;
 
-unsigned int number_of_paper_rolls(const char *input_data, const size_t input_data_length, const char target_char) {
-    unsigned int count = 0;
+int number_of_paper_rolls(const char *input_data, const size_t input_data_length, const char target_char) {
+    int count = 0;
     for (size_t i = 0; i < input_data_length; ++i) {
         if (input_data[i] ==  target_char) {
             count++;
@@ -19,9 +23,9 @@ unsigned int number_of_paper_rolls(const char *input_data, const size_t input_da
 }
 
 void create_paper_rolls(const char *input_data, const size_t input_data_length, PaperRoll *paper_rolls, const char target_char) {
-    unsigned int row = 0;
-    unsigned int col = 0;
-    unsigned int paper_roll_index = 0;
+    int row = 0;
+    int col = 0;
+    int paper_roll_index = 0;
     
     for (size_t i = 0; i < input_data_length; ++i) {
         char current_char = input_data[i];
@@ -37,6 +41,45 @@ void create_paper_rolls(const char *input_data, const size_t input_data_length, 
             col++;
         }
     }
+}
+
+int number_of_adjacent_paper_rolls(PaperRoll *all_paper_rolls, int all_paper_rolls_count, PaperRoll* paper_roll) {
+    PaperRoll paper_rolls_of_interest[ADJACENT_FLAT_POSITIONS_COUNT] = {
+        {paper_roll->row_position - 1, paper_roll->column_position - 1, 0},
+        {paper_roll->row_position - 1, paper_roll->column_position, 0},
+        {paper_roll->row_position - 1, paper_roll->column_position + 1, 0},
+        {paper_roll->row_position,     paper_roll->column_position - 1, 0},
+        {paper_roll->row_position,     paper_roll->column_position + 1, 0},
+        {paper_roll->row_position + 1, paper_roll->column_position - 1, 0},
+        {paper_roll->row_position + 1, paper_roll->column_position, 0},
+        {paper_roll->row_position + 1, paper_roll->column_position + 1, 0}
+    };
+
+    int adjacent_count = 0;
+    for (int i = 0; i < all_paper_rolls_count; ++i) {
+        for (int j = 0; j < ADJACENT_FLAT_POSITIONS_COUNT; ++j) {
+            if (all_paper_rolls[i].row_position == paper_rolls_of_interest[j].row_position &&
+                all_paper_rolls[i].column_position == paper_rolls_of_interest[j].column_position) {
+                adjacent_count++;
+                break;
+            }
+        }
+    }
+
+    paper_roll->adjacent_count = adjacent_count;
+
+    return adjacent_count;
+}
+
+void detect_paper_rolls_to_remove(PaperRoll *all_paper_rolls, int all_paper_rolls_count, int* paper_roll_indexes_to_remove, int* removal_count) {
+    int count = 0;
+    for (int i = 0; i < all_paper_rolls_count; ++i) {
+        if (all_paper_rolls[i].adjacent_count < ADJACENT_THRESHOLD) {
+            paper_roll_indexes_to_remove[count] = i;
+            count++;
+        }
+    }
+    *removal_count = count;
 }
 
 #endif
--- 5/c/main.c
+++ 5/c/main.c
@@ -6,12 +6,13 @@
 
 #define TARGET_CHAR '@'
 
-void test_paper_roll_detection() {
-    char input[] = {'.', TARGET_CHAR, '.' };
+void test_paper_roll_detection()
+{
+    char input[] = {'.', TARGET_CHAR, '.'};
     size_t input_length = sizeof(input) / sizeof(input[0]);
 
-    unsigned int expected_count = 1;
-    unsigned int actual_count = number_of_paper_rolls(input, input_length, TARGET_CHAR);
+    int expected_count = 1;
+    int actual_count = number_of_paper_rolls(input, input_length, TARGET_CHAR);
     assert((expected_count == actual_count) && "Test failed: Expected 1 paper roll.");
 
     char input_multiline[] = {'.', TARGET_CHAR, '.', '\n', TARGET_CHAR, TARGET_CHAR, '.', '\n', '.', '.', TARGET_CHAR};
@@ -22,20 +23,21 @@ void test_paper_roll_detection() {
     assert((expected_count == actual_count) && "Test failed: Expected 4 paper rolls.");
 }
 
-void test_paper_roll_creation() {
-    char input[] = {'.', TARGET_CHAR, '.' };
+void test_paper_roll_creation()
+{
+    char input[] = {'.', TARGET_CHAR, '.'};
     size_t input_length = sizeof(input) / sizeof(input[0]);
 
-    unsigned int number_of_paper_rolls_count = number_of_paper_rolls(input, input_length, TARGET_CHAR);
+    int number_of_paper_rolls_count = number_of_paper_rolls(input, input_length, TARGET_CHAR);
     PaperRoll paper_rolls[number_of_paper_rolls_count];
 
     create_paper_rolls(input, input_length, paper_rolls, TARGET_CHAR);
-    
+
     assert((paper_rolls[0].row_position == 0) && (paper_rolls[0].column_position == 1) && "Test failed: Paper roll position incorrect.");
 
     char input_multiline[] = {'.', TARGET_CHAR, '.', '\n', TARGET_CHAR, TARGET_CHAR, '.', '\n', '.', '.', TARGET_CHAR};
     input_length = sizeof(input_multiline) / sizeof(input_multiline[0]);
-    
+
     number_of_paper_rolls_count = number_of_paper_rolls(input_multiline, input_length, TARGET_CHAR);
     PaperRoll paper_rolls_multi[number_of_paper_rolls_count];
 
@@ -47,31 +49,101 @@ void test_paper_roll_creation() {
     assert((paper_rolls_multi[3].row_position == 2) && (paper_rolls_multi[3].column_position == 2) && "Test failed: Fourth paper roll position incorrect.");
 }
 
-void run_all_tests() {
+void test_adjacent_paper_rolls()
+{
+    PaperRoll paper_roll = {0, 1, 0};
+    PaperRoll all_paper_rolls[] = {
+        {0, 1, 0},
+        {0, 2, 0}};
+
+    int all_paper_rolls_count = 1;
+    int number_of_paper_rolls_count = number_of_adjacent_paper_rolls(all_paper_rolls, all_paper_rolls_count, &paper_roll);
+
+    assert((number_of_paper_rolls_count == 0) && "Test failed: Expected 0 adjacent paper rolls.");
+    assert((paper_roll.adjacent_count == 0) && "Test failed: Paper roll adjacent count incorrect.");
+
+    all_paper_rolls_count = 2;
+    number_of_paper_rolls_count = number_of_adjacent_paper_rolls(all_paper_rolls, all_paper_rolls_count, &paper_roll);
+
+    assert((number_of_paper_rolls_count == 1) && "Test failed: Expected 1 adjacent paper roll.");
+    assert((paper_roll.adjacent_count == 1) && "Test failed: Paper roll adjacent count incorrect.");
+
+    PaperRoll fully_adjacent_rolls[] = {
+        {0, 0, 0}, {0, 1, 0}, {0, 2, 0}, {1, 0, 0}, {1, 1, 0}, {1, 2, 0}, {2, 0, 0}, {2, 1, 0}, {2, 2, 0}};
+    PaperRoll fully_adjacent_roll = {1, 1, 0};
+
+    all_paper_rolls_count = 9;
+    number_of_paper_rolls_count = number_of_adjacent_paper_rolls(fully_adjacent_rolls, all_paper_rolls_count, &fully_adjacent_roll);
+
+    assert((number_of_paper_rolls_count == 8) && "Test failed: Expected 8 adjacent paper rolls.");
+    assert((fully_adjacent_roll.adjacent_count == 8) && "Test failed: Fully adjacent paper roll adjacent count incorrect.");
+}
+
+void test_removal_of_paper_rolls()
+{
+    PaperRoll all_paper_rolls[] = {
+        {0, 1, 0},
+        {0, 2, 0}
+    };
+
+    int all_paper_rolls_count = 2;
+    int paper_roll_indexes_to_remove[all_paper_rolls_count];
+    int removal_count = 0;
+
+    detect_paper_rolls_to_remove(all_paper_rolls, all_paper_rolls_count, paper_roll_indexes_to_remove, &removal_count);
+
+    assert((removal_count == 2) && "Test failed: Paper rolls were not removed.");
+}
+
+void run_all_tests()
+{
 
     test_paper_roll_detection();
     test_paper_roll_creation();
+    test_adjacent_paper_rolls();
+    test_removal_of_paper_rolls();
 
     printf("\n***\n");
     printf("All tests passed!\n");
     printf("***\n\n");
 }
 
-int main() {
-
+int main()
+{
     run_all_tests();
-
-    return 0;
 
     size_t buffer_size = 0;
     char *buffer = create_buffer_for_file("../input_small.txt", &buffer_size);
 
-    if (buffer == NULL) {
+    if (buffer == NULL)
+    {
         return -1;
     }
 
     int file_read_status = read_file_contents_into_buffer("../input_small.txt", buffer, buffer_size);
-    
+
+    if (file_read_status < 0)
+    {
+        free(buffer);
+        return -1;
+    }
+
+    int number_of_paper_rolls_count = number_of_paper_rolls(buffer, buffer_size, TARGET_CHAR);
+    PaperRoll paper_rolls[number_of_paper_rolls_count];
+
+    create_paper_rolls(buffer, buffer_size, paper_rolls, TARGET_CHAR);
+
+    for (int i = 0; i < number_of_paper_rolls_count; ++i) {
+        number_of_adjacent_paper_rolls(paper_rolls, number_of_paper_rolls_count, &paper_rolls[i]);
+    }
+
+    int paper_roll_indexes_to_remove[number_of_paper_rolls_count];
+    int removal_count = 0;
+
+    detect_paper_rolls_to_remove(paper_rolls, number_of_paper_rolls_count, paper_roll_indexes_to_remove, &removal_count);
+
+    printf("Number of paper rolls to remove: %d\n", removal_count);
+
     free(buffer);
 
     return 0;
