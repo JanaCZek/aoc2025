2c8551b47d83acbed142d5b660c38a118a7200d1
IN\janecka <tomas.janecka@thermofisher.com>
2025-12-04 09:55:58 +0100

    Testing initial idea

--- 5/c/main.c
+++ 5/c/main.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <assert.h>
+
+int add(int a, int b) {
+    return a - b;
+}
+
+void test_add() {
+    assert(add(2, 3) == 5);
+    assert(add(-1, 1) == 0);
+    assert(add(0, 0) == 0);
+    assert(add(-2, -3) == -5);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    test_add();
     return 0;
 }
