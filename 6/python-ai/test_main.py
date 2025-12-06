c97327529c7ed87cf0e97c1774db666d7d6d1a62
IN\janecka <tomas.janecka@thermofisher.com>
2025-12-06 07:22:23 +0100

    Day 6 part two

--- 6/python-ai/test_main.py
+++ 6/python-ai/test_main.py
@@ -10,8 +10,15 @@ def test_cephlapod_math():
     6 98  215 314
 *   +   *   +  """
         result = cephlapod_math(input)
-        print("cephlapod_math result:", result)
         assert result == 4277556
+
+def test_cephlapod_math_part_two():
+        input = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """
+        result = cephlapod_math_part_two(input)
+        assert result == 3263827
 
 def cephlapod_math(input: str) -> int:
     # Split input into lines and columns
@@ -42,10 +49,49 @@ def cephlapod_math(input: str) -> int:
         result += val
     return result
 
-def test_count_fresh_ingredient_ids_final():
+def cephlapod_math_part_two(input: str) -> int:
+    lines = input.strip().split('\n')
+    max_index = len(lines[0])
+    max_height = 4
+    current_operator = None
+    results = []
+    numbers = []
+
+    for index in range(0, max_index):
+        if index < len(lines[-1]) and lines[-1][index] in ('+', '*'):
+            current_operator = lines[-1][index]
+        number_str = ""
+        for line in lines[:-1]:
+            if line[index].isdigit():
+                number_str += line[index]
+        if number_str.isdigit():
+            numbers.append(int(number_str))
+        if ((index + 1) < len(lines[-1]) and lines[-1][index + 1] in ('+', '*')) or index == max_index - 1:
+            if current_operator == '+':
+                col_result = sum(numbers)
+            elif current_operator == '*':
+                col_result = 1
+                for num in numbers:
+                    col_result *= num
+            else:
+                col_result = 0
+            results.append(col_result)
+            numbers = []
+    final_result = sum(results)
+    
+    return final_result
+
+def test_input_part_one():
     with open(r"c:/Projects/playground/aoc2025/6/input.txt", encoding='utf-8') as f:
         input_str = f.read()
         result = cephlapod_math(input_str)
+        print("Final result:", result)
+    assert True
+    
+def test_input_part_two():
+    with open(r"c:/Projects/playground/aoc2025/6/input.txt", encoding='utf-8') as f:
+        input_str = f.read()
+        result = cephlapod_math_part_two(input_str)
         print("Final result:", result)
     assert True
     
