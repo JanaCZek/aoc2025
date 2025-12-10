8d2f7b9c9b92b8c4796c2c755d369bc6fe304634
IN\janecka <tomas.janecka@thermofisher.com>
2025-12-10 09:46:50 +0100

    Day 10 part one

--- 10/python/test_main.py
+++ 10/python/test_main.py
@@ -69,21 +69,50 @@ def test_parse_line_to_machine():
     assert desired_state == expected_desired_state
     assert button_sets == expected_button_sets
 
+def test_toggle():
+    button_sets = [
+        [3],
+        [1, 3],
+        [2],
+        [2, 3],
+        [0, 2],
+        [0, 1],
+    ]
+    current_state = [False, False, False, False]
+    desired_state = [False, True, True, False]
+
+    new_state = toggle([button_sets[-1], button_sets[-2]], current_state)
+    assert new_state == desired_state
+
+def test_fitness_function():
+    current_state = [False, False, False, False]
+    desired_state = [False, True, True, False]
+
+    button_press_count = 2
+    best_score = fitness_function(button_press_count, current_state, desired_state)
+
+    button_press_count = 3
+    worse_score = fitness_function(button_press_count, current_state, desired_state)
+
+    assert best_score > worse_score
+
+
 def hillclimber_button_press_count(button_sets, current_state, desired_state):
     # button_sets: [[0, 1, 2], [1, 3], ...]
     # current_state: [true, false, ...]
 
-    population_size = 10
+    population_size = 100
     population = create_population(button_sets, population_size, None)
-    generations_count = 20
+    generations_count = 500
     generation = 0
     retry_count = 0
 
     global_best_score = -np.inf
     global_best_individual = None
+    global_minimum_button_press_count = np.inf
     result_found = False
 
-    while not result_found:
+    while not result_found and generation < generations_count:
         fitness_scores = []
 
         for individual in population:
@@ -94,20 +123,23 @@ def hillclimber_button_press_count(button_sets, current_state, desired_state):
 
             fitness_scores.append(score)
 
-        best_index = np.argmax(fitness_scores)
+        best_index = max(range(len(fitness_scores)), key=lambda i: fitness_scores[i])
         best_individual = population[best_index]
         best_score = fitness_scores[best_index]
 
-        if best_score > global_best_score and (global_best_individual is None or len(best_individual) <= len(global_best_individual)):
+        if best_score >= global_best_score:
             global_best_score = best_score
             global_best_individual = best_individual
-            print(f"Generation {generation}: New best individual found with button press count {global_best_individual}")
+
+        if len(best_individual) < global_minimum_button_press_count:
+            global_minimum_button_press_count = len(best_individual)
 
         population = create_population(button_sets, population_size, global_best_individual)
 
         if generation == generations_count - 1:
             new_state = toggle(global_best_individual, current_state)
             if new_state != desired_state:
+                result_found = False
                 generations_count += 10
                 if retry_count > 10:
                     population = create_population(button_sets, population_size, None)
@@ -140,7 +172,7 @@ def toggle(button_sets, current_state):
     return new_state
 
 def create_population(button_sets, population_size, best_individual):
-    stddev_size = 1
+    stddev_size = 3
 
     # Generate button set counts up to len(button_sets) using normal distribution around best_individual
     button_set_count = np.random.randint(1, len(button_sets), size=population_size)
@@ -195,17 +227,9 @@ def test_input_part_one():
         for line in f:
             desired_state, button_sets = parse_line_to_machine(line.strip())
             current_state = [False] * len(desired_state)
-            best_button_press_count, _ = hillclimber_button_press_count(button_sets, current_state, desired_state)
+            best_button_press_count, best_individual = hillclimber_button_press_count(button_sets, current_state, desired_state)
             print("Best button press count found for line:", best_button_press_count)
             total_counts += best_button_press_count
         print("Final total button press counts:", total_counts)
             
     assert True
-    
-# def test_input_part_two():
-#     with open(r"c:/Projects/playground/aoc2025/10/input.txt", encoding='utf-8') as f:
-#         input_str = f.read()
-#         result = product_of_last_two_x_coordinates(input_str)
-#         print("Final result:", result)
-#     assert True
-    
